df1147f34f76f8f75053d651168bbd71998fb0db
Rashid <156198021+rashidvaloringal@users.noreply.github.com>
2026-07-08 17:17:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // 🔐 നിങ്ങളുടെ Supabase credentials ഇവിടെ പേസ്റ്റ് ചെയ്യുക
 // Supabase Dashboard → Project Settings → API
 
-window.ENV_SUPABASE_URL = "https://axjeqkeocbfemojomfjm.supabase.co";
-window.ENV_SUPABASE_KEY = "sb_publishable_JgdXpn2s46NfLu0wNHU_9w_Cj9ewUKE";
+window.ENV_SUPABASE_URL = "https://axjeqkeocbfemojomfj.supabase.co";
+window.ENV_SUPABASE_KEY = "sb_publishable_JgdXpn2s46NfLu0wNHU_9_Cj9ewUKE";
 // പബ്ലിക് എഡ്ജ് വോയിസ് ഗേറ്റ്‌വേ ലിങ്ക്
 window.ENV_EDGE_TTS_API = "https://edge-tts-proxy.vercel.app/api/tts";
